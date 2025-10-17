0ac64039ebd6e1924ae1dbad88cc4b1d8941d94e
MatthewEnamuotor <90224141+oviematthew@users.noreply.github.com>
2025-10-17 18:55:54 -0400

    updates to contact section

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -22,6 +22,7 @@ export default function ContactSection() {
           </a>{" "}
           alternatively, text me on{" "}
           <a
+            target="_blank"
             href="https://www.linkedin.com/in/matthew-ovie-enamuotor-9992b6132/"
             className="text-brand hover:text-brand/90 transition hover:underline"
           >
